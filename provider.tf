e0a11fbdbe4ad97598282a584da1e90779a39619
Ibrahim <ibra.653@gmail.com>
2025-07-10 19:10:09 +0500

    updated

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-      /* Multiple environments will be used, each with .tfvars file.
+      /* Multiple environments will be used, each with backend.tfvars file.
       bucket       = "abra-terraform-states"
       key          = "kubernetes/terraform.tfstate"
       region       = "us-east-1"
